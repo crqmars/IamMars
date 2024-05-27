459ef24355848f6f2f44d33a4432795522f9d00d
陈若青 <chenrq@xmpaoyou.com>
2024-05-27 14:48:43 +0800

    test

--- Mars/Program.cs
+++ Mars/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                var a = Console.ReadLine();
+            }
         }
     }
 }
